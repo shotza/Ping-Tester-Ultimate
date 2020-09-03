5e26dd29c7a3b5f90a662544582930653df97b81
shota_zaali90 <shota_zaali90@hotmail.com>
2020-09-03 18:35:38 +0400

    Change

---
+++ pingTester/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace pingTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cMDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.systemLabel = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SystemLab1 = new System.Windows.Forms.Label();
+            this.ipAdd = new System.Windows.Forms.TextBox();
+            this.pingBtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.cMDToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(990, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // cMDToolStripMenuItem
+            // 
+            this.cMDToolStripMenuItem.Name = "cMDToolStripMenuItem";
+            this.cMDToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.cMDToolStripMenuItem.Text = "CMD";
+            this.cMDToolStripMenuItem.Click += new System.EventHandler(this.cMDToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(25, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(307, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Status";
+            // 
+            // systemLabel
+            // 
+            this.systemLabel.AutoSize = true;
+            this.systemLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.systemLabel.Location = new System.Drawing.Point(307, 186);
+            this.systemLabel.Name = "systemLabel";
+            this.systemLabel.Size = new System.Drawing.Size(33, 25);
+            this.systemLabel.TabIndex = 3;
+            this.systemLabel.Text = "---";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statusLabel.Location = new System.Drawing.Point(307, 94);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(33, 25);
+            this.statusLabel.TabIndex = 4;
+            this.statusLabel.Text = "---";
+            // 
+            // SystemLab1
+            // 
+            this.SystemLab1.AutoSize = true;
+            this.SystemLab1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SystemLab1.Location = new System.Drawing.Point(307, 146);
+            this.SystemLab1.Name = "SystemLab1";
+            this.SystemLab1.Size = new System.Drawing.Size(84, 25);
+            this.SystemLab1.TabIndex = 5;
+            this.SystemLab1.Text = "System";
+            // 
+            // ipAdd
+            // 
+            this.ipAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ipAdd.Location = new System.Drawing.Point(30, 96);
+            this.ipAdd.Name = "ipAdd";
+            this.ipAdd.Size = new System.Drawing.Size(206, 30);
+            this.ipAdd.TabIndex = 6;
+            // 
+            // pingBtn
+            // 
+            this.pingBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pingBtn.Location = new System.Drawing.Point(30, 146);
+            this.pingBtn.Name = "pingBtn";
+            this.pingBtn.Size = new System.Drawing.Size(206, 39);
+            this.pingBtn.TabIndex = 7;
+            this.pingBtn.Text = "PING";
+            this.pingBtn.UseVisualStyleBackColor = true;
+            this.pingBtn.Click += new System.EventHandler(this.pingBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 247);
+            this.Controls.Add(this.pingBtn);
+            this.Controls.Add(this.ipAdd);
+            this.Controls.Add(this.SystemLab1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.systemLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Ping Tester Ultimate (BOG) v1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cMDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label systemLabel;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label SystemLab1;
+        private System.Windows.Forms.TextBox ipAdd;
+        private System.Windows.Forms.Button pingBtn;
+    }
+}
+
---
+++ pingTester/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace pingTester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Shota Abzhandadze for Bank of Georgia");
+        }
+
+        private void cMDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("CMD.exe");
+        }
+        private void textBox1_Validated(object sender, EventArgs e)
+        {
+
+        }
+        private void pingBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(ipAdd.Text) || ipAdd.Text == "")
+            {
+                MessageBox.Show("Please use valid IP or web address!");
+            }
+            Ping p = new Ping();
+            PingReply r;
+            string s;
+            s = ipAdd.Text;
+            r = p.Send(s);
+            if (r.Status == IPStatus.Success)
+            {
+                statusLabel.Text = "Ping to " + s.ToString() + "[" + r.Address.ToString() + "]" + " Successful"
+                   + " Response delay = " + r.RoundtripTime.ToString() + " ms" + "\n";
+
+                systemLabel.Text = r.Options.Ttl.ToString();
+                int yourInt = int.Parse(systemLabel.Text);
+                if (yourInt > 64){
+                    systemLabel.Text = "Windows";
+                } else
+                {
+                    systemLabel.Text = "Linux";
+                }
+            }
+            else if (r.Status == IPStatus.TimedOut)
+            {
+                statusLabel.Text = "Destination TimeOut";
+            }
+
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ipAdd.Text = "";
+            statusLabel.Text = "---";
+            systemLabel.Text = "---";
+        }
+    }
+}
